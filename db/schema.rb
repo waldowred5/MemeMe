362387a3edc3a18b6937e5e874ca2b528eb38eee
DK <dcsk.daniel@gmail.com>
2020-11-02 14:22:47 +1100

    updated memes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_010250) do
+ActiveRecord::Schema.define(version: 2020_11_02_012051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memes", force: :cascade do |t|
+    t.text "name"
+    t.bigint "user_id", null: false
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_memes_on_user_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
@@ -32,6 +41,6 @@ ActiveRecord::Schema.define(version: 2020_11_02_010250) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
 
+  add_foreign_key "memes", "users"
 end
